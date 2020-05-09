8869c318cf2f4f500e4284289e1332cf912b88f5
azwri <aazwri@gmail.com>
2020-05-09 05:48:16 +0300

    add javascript tutorial

---
+++ dart-tutorial/02-basic.dart
@@ -0,0 +1,15 @@
+void main(){
+  print("My name is ${name}");
+  print("Number is ${number}");
+}
+
+String name = 'Abdulrahman';
+int number = 1000000;
+
+// This is a comment
+/*
+This is
+multi
+comments
+*/
+
